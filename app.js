66cd4a0e0ba10fb4d26110779d0c1bac2f63236d
MichaelSilski <sil16005@byui.edu>
2022-02-01 11:13:14 -0700

    Updated UserId

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  User.findById('61f2d69245a1be3d50fac5ef')
     .then(user => {
       req.user = user;
       next();
