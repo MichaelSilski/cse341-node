23bd7c36c8ea5f28abe260fb5a88763e78e617db
MichaelSilski <sil16005@byui.edu>
2022-02-01 10:36:14 -0700

    Added semicolon

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
